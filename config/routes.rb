a14552cda6aae747b0a777ce4460886dd6d87946
Adrian Wright <agwright72@gmail.com>
2018-07-26 15:04:09 +0700

    kind of finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'questions/ask'
+  #get 'questions/answer'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
 end
